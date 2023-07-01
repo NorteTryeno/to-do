53f006da7674e636510d048176a04a0f764bb3f6
NorteTryeno <wellkor@yandex.ru>
2023-07-01 18:57:53 +0400

    updated configuration and main file

--- src/main.py
+++ src/main.py
@@ -3,10 +3,4 @@ from fastapi import FastAPI
 from src.posts.router import router as router_task
 app = FastAPI()
 
-
-@app.get('/')
-def init():
-    return {"200": "Hello, World!"}
-
-
 app.include_router(router_task)
